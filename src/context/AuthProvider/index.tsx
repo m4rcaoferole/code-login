0c5284346684aa78125d54bda8c5f9c75dc11296
Marcos Vinicius <victor_jv380@hotmiail.com>
2022-05-15 21:56:13 -0400

    feat: get set localstorage

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -1,21 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { IAuhtProvider, IContext, IUser } from "./types";
-import { LoginRequest } from "./util";
+import { getUserLocalStorage, LoginRequest, setUserLocalStorage } from "./util";
 
 export const AuthContext = createContext<IContext>({} as IContext); // enteder que o obj não sej do formato
 
 export const AuthProvider = ({ children }: IAuhtProvider) => { // Controlador do estado
    const [ user, setUser ] = useState<IUser | null>()
 
+   useEffect(() => {
+      const user = getUserLocalStorage()
+
+      if(user) setUser(user)
+   }, [])
+
    async function authenticate({ email, password }: IUser) {
       const response = await LoginRequest({ email, password });
       const payload = { token: response.token, email } // User autenticado
 
       setUser(payload)
+      setUserLocalStorage(payload)
    }
 
    function logout() {
       setUser(null)
+      setUserLocalStorage(null)
    }
 
    return (
