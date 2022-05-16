2bf3578d0b1e905db9af56f64058ce7c9869de2a
Marcos Vinicius <victor_jv380@hotmiail.com>
2022-05-15 21:56:39 -0400

    feat: get set localstorage

--- src/context/AuthProvider/util.ts
+++ src/context/AuthProvider/util.ts
@@ -1,6 +1,19 @@
 import { Api } from "../../services/api";
 import { IUser } from "./types";
 
+export function setUserLocalStorage(user: IUser | null) { // Salvar os items no Local Storage
+   localStorage.setItem("u", JSON.stringify(user));
+}
+
+export function getUserLocalStorage() {
+   const json = localStorage.getItem("u")
+
+   if(!json) return null;
+
+   const user = JSON.parse(json);
+   return user ?? null;
+}
+
 export async function LoginRequest ({email, password}: IUser) {
    try {
       const request = await Api.post("login", { email, password });
